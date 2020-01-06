65eb985b3c8547ce352d919a9af0b36f5e9b8fa8
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-06 03:44:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+require 'pry'
+class Anagram 
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end  
+  
+  def match(array)
+    array.keep_if {|string| name.split('').sort == string.split('').sort}
+  end
+
+end 
